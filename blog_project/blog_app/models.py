d086cc77f8792339f02b557724510bd61706c1de
Geonyeong <renmsts@naver.com>
2023-09-15 23:16:21 +0900

    add models.py views.py(post_list, BlogPostList)

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='전체')
+    temporary = models.CharField(max_length=1, default='Y') # 임시저장
+    views = models.PositiveIntegerField(default=0)
+    author_id = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
+    
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BlogPost
+from rest_framework import generics
+from .serializers import BlogPostSerializer
 
 # Create your views here.
+def post_list(request):
+    category = request.GET.get('category')
+    if category:
+        posts = BlogPost.objects.filter(category=category, publish='Y').order_by('-views')
+    else:
+        posts = BlogPost.objects.filter(publish='Y').order_by('-views')
+    
+    context = {
+        'posts: posts',
+    }
+    return render(request, 'post_list.html', context)
+
+# 포스트 restful api 뷰 생성
+class BlogPostList(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
