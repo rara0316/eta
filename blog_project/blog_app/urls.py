651acdfc184dacc4957cf280a5f80ff00b1667be
animalwook <youngwook616@gmail.com>
2023-09-18 11:45:23 +0900

    필터기능 구현, 임시 포스트조회수

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('',views.post_list, name='post_list'),
     path('post_list/<str:topic>/', views.post_list, name='post_list_by_topic'),
 
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+
     path('login/', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -14,13 +14,6 @@ from .serializers import BlogPostSerializer
 
 # 첫화면으로 보여줄 화면입니다. post_list를 대신해 임시로 넣었습니다.
 # post_list.html링크를 수정할 필요가 있습니다.
-def index(request):
-    # 게시글 오브젝트
-    BlogPosts = BlogPost.objects.all() 
-    posts = {'posts':BlogPosts}
-    return render(request,'index.html', posts)
-
-
 def login_view(request):
     if request.user.is_authenticated:
         return redirect('blog_app:post_list')
@@ -42,6 +35,19 @@ def login_view(request):
 
     return render(request, 'login.html', {'form': form})
 
+def post_list(request,topic=None):
+    if topic:
+        posts = BlogPost.objects.filter(topic=topic, publish='Y').order_by('-views')
+    else:
+        posts = BlogPost.objects.filter(publish='Y').order_by('-views')
+    # dict형식을 잘못 작성하고 있었습니다.
+    posts = {
+        'posts': posts,
+    }
+    return render(request, 'post_list.html', posts)
+
+
+
 def write(request):
     form = BlogPostForm()
     return render(request, 'write.html', {'form': form})
@@ -61,19 +67,7 @@ class image_upload(View):
         # 이미지 업로드가 끝나면 JSON으로 이미지 파일 url 변환
         return JsonResponse({'location': file_url})
 
-def post_list(request,topic=None):
-    category = request.GET.get('category')
-    if category:
-        posts = BlogPost.objects.filter(category=category, publish='Y').order_by('-views')
-    else:
-        posts = BlogPost.objects.filter(publish='Y').order_by('-views')
-    posts = BlogPost.objects.filter(publish='Y').order_by('-views')
 
-    # dict형식을 잘못 작성하고 있었습니다.
-    posts = {
-        'posts': posts,
-    }
-    return render(request, 'post_list.html', posts)
 
 # 포스트 restful api 뷰 생성
 class BlogPostList(generics.ListCreateAPIView):
@@ -99,7 +93,7 @@ def post(self, request, post_id=None):
         if 'delete-button' in request.POST:
             post.delete()
             return redirect('blog_app:post_list')
-        post.category = '전체' if not form.cleaned_data.get('topic') else form.cleaned_data.get('topic')
+        post.topic = '전체' if not form.cleaned_data.get('topic') else form.cleaned_data.get('topic')
         post.publish = 'N' if 'temp-save-button' in request.POST else 'Y'
         post.author_id = request.user.username
         post.save()
@@ -108,3 +102,16 @@ def post(self, request, post_id=None):
     context = {'form': form, 'post':post, 'edit_mode':post_id is not None, 'MEDIA_URL':settings.MEDIA_URL}
     return render(request, self.template_name, context)
             
+def post_detail(request, post_id):
+    # 포스트 id로 게시물 가져옴
+    post = get_object_or_404(BlogPost, id=post_id)
+
+    # 조회수 증가 및 db에 저장
+    post.views += 1 
+    post.save() 
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'post.html', context)
