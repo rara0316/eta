14c27e8c76f6393091bf2eb09f24b0667d3848b6
Geonyeong <renmsts@naver.com>
2023-09-19 11:40:33 +0900

    update

--- blog_project/blog_app/forms.py
+++ blog_project/blog_app/forms.py
@@ -13,7 +13,7 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         exclude = ['created_at']
-        fields = ['title', 'content']
+        fields = ['title', 'content','topic']
 
     def __init__(self, *args, **kwargs):
         super(forms.ModelForm, self).__init__(*args, **kwargs)
