543cb191487c252b68f844812d4522ee77b8a9c1
animalwook <youngwook616@gmail.com>
2023-09-19 18:21:05 +0900

    댓글 병합

--- blog_project/blog_app/forms.py
+++ blog_project/blog_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import BlogPost
+from .models import BlogPost, Comment
 from django_ckeditor_5.widgets import CKEditor5Widget
 
 class CustomLoginForm(forms.Form):
@@ -25,3 +25,19 @@ class BlogPostForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields["title"].required = False
         self.fields["content"].required = False        
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        labels = {
+            'text': '',  # 'text' 필드의 라벨을 빈 문자열로 설정
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({
+            'class': 'form-text',
+            'rows': '4',
+            'cols': '100',
+        })
